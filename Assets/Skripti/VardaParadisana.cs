e288e411e73658f3cc6170f9e6a29248e246b133
Travinovs <aleksandrs.travinovs@gmail.com>
2023-03-29 09:19:25 +0300

    Izveidots pirmais skripts

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour {
+
+	private string teksts;
+	private string[] varianti = { "Sveiks", "Jauku dienu!", "Prieks tevi redzet!", "Uzredzesanos" };
+
+	int nejausais;
+	public GameObject ievadesLauks;
+	public GameObject tekstaAttelosana;
+
+	public void UzglabatTekstu() {
+		nejausais = Random.Range (0, varianti.Length);
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		tekstaAttelosana.GetComponent<Text>().text = varianti[nejausais]+teksts.ToUpper()+"!";
+	}
+
+}
