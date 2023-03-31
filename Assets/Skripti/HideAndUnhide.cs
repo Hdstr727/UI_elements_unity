d32be48cfc3ddd87d919284a1114e9e5df042492
Travinovs <aleksandrs.travinovs@gmail.com>
2023-03-31 09:19:34 +0300

    Izveidoti checkboxi un skripts HideAndUnhide

---
+++ Assets/Skripti/HideAndUnhide.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideAndUnhide : MonoBehaviour {
+    public GameObject bean;
+    public GameObject lacis;
+    public GameObject oma;
+    public GameObject masina;
+    
+    public void BeanAttels(bool vertiba) { 
+        bean.SetActive(vertiba);
+    }
+
+    public void LacisAttels(bool vertiba)
+    {
+        lacis.SetActive(vertiba);
+    }
+
+    public void OmaAttels(bool vertiba)
+    {
+        oma.SetActive(vertiba);
+    }
+
+    public void MasinaAttels(bool vertiba)
+    {
+        masina.SetActive(vertiba);
+    }
+
+
+}
